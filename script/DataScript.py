db78d8079937be13288633ca1b8b581ff266bc53
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-10-19 13:37:28 +0530

    Bubble Sort

--- script/DataScript.py
+++ script/DataScript.py
@@ -1,7 +1,6 @@
 get_bin = lambda x,n : ''.join(reversed( [str((x >> i) & 1) for i in range(n)] ) )
-
-fout = open('DataMemory.mif','w')
 n = int(raw_input('Number of elementts: '))
+fout = open('DataMemory.mif','w')
 fout.write(get_bin(n,32)+'\n')
 nos = map(int,raw_input().strip('\n').split(' '))
 for no in nos:
